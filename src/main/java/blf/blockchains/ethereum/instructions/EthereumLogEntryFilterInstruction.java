bc904b1637e46fb80c598fc923b9cf84eda76387
Paul Beck <paulbeck@Pauls-MacBook-Pro.local>
2022-01-11 22:05:34 +0100

    address

--- src/main/java/blf/blockchains/ethereum/instructions/EthereumLogEntryFilterInstruction.java
+++ src/main/java/blf/blockchains/ethereum/instructions/EthereumLogEntryFilterInstruction.java
@@ -77,7 +77,9 @@ public class EthereumLogEntryFilterInstruction extends Instruction {
     }
 
     private boolean isValidLogEntry(ProgramState state, EthereumLogEntry logEntry) {
-        return this.contractCriterion.test(state, logEntry.getAddress()); //&& this.signature.hasSignature(logEntry);
+        String address = (String) state.getValueStore().getValue("log.address");
+        LOGGER.info("Compare " + address + " and " + logEntry.getAddress());
+        return  address == logEntry.getAddress(); //&& this.signature.hasSignature(logEntry);
     }
 
     private List<EthereumLogEntry> getEntries(ProgramState state) {
